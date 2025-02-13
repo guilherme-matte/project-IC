9a7d0e1a509f57f86d76fe43c9171308037eff92
guilherme-matte <guiguimatte@gmail.com>
2025-02-13 13:39:18 -0300

    CorsConfig criado

---
+++ src/main/java/control/invest/IC/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package control.invest.IC.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+
+@EnableWebMvc
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Permite qualquer origem
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Permite todos os headers
+                        .allowCredentials(false);
+            }
+        };
+    }
+}
